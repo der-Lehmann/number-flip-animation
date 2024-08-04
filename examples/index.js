6efb10550f48d91442e1fdb7f4689d74a7b40bd0
larslehmann <larsh.lehmann64@gmail.com>
2024-08-04 15:25:38 +0200

    Added an examples folder and a minimal example to show the usage of the package. To make the examples easily available when cloning the repository, the http-server package has been added as a dev dependency. Created a command in package.json that makes the examples available locally.

---
+++ examples/index.js
@@ -0,0 +1,10 @@
+import { NumberFlip } from './dist/index.js';
+
+const numberFlip = new NumberFlip({ rootElement: document.getElementById('number-flip') });
+
+function setNumber() {
+  numberFlip.setNumberTo({ newNumber: Math.floor(Math.random() * 10000) });
+}
+
+window.setInterval(setNumber, 2000);
+setNumber();
